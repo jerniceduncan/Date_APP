cc5cbfcf826b213ec28de497509983c63bfd6301
Jernice <jerniceduncan@icloud.com>
2021-12-01 16:35:30 -0800

    updated nav_bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,13 +3,15 @@ import styled from "styled-components";
 const Navigation = styled.div`
   height: 75px;
   width: 100%;
-  background-color: blue;
-  box-shadow: 20px #888888;
+  background-color: #17A2B8;
+  box-shadow: 10px #888888;
+ 
+
 `;
 const Nav = () => {
   return (
     <div>
-      <Navigation></Navigation>
+      <Navigation>Vibes</Navigation>
     </div>
   );
 };
