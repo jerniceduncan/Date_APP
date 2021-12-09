1ba60d3d8a576f82f48cf805f0c29adbcc963d54
Jernice <jerniceduncan@icloud.com>
2021-12-09 10:56:00 -0800

    added Stripe feature

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
-
 const SignUp = () => {
-
-const {
+  const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
   console.log(errors);
-  
-  
-  
+
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
