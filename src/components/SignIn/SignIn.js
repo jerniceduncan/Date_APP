8c8d46aa72ab080975440a7d11acec0778853a0d
Jernice <jerniceduncan@icloud.com>
2022-01-27 10:25:23 -0800

    added signup

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import UserContext from "../Context/UserContext";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
 
-const SignIn = () => {
+const Login = () => {
+  const { userData, setUserData } = useContext(UserContext);
+  const [form, setForm] = useState({});
+  const history = useHistory();
+
+  const onChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const loginRes = await Axios.post("/users/login", {
+        email: form.email,
+        password: form.password,
+      });
+
+      setUserData({
+        token: loginRes.data.token,
+        user: loginRes.data.user,
+      });
+
+      localStorage.setItem("auth-token", loginRes.data.token);
+      history.push("/");
+    } catch (err) {
+      console.log("problem", err);
+    }
+  };
+
+  useEffect(() => {
+    if (userData.user) history.push("/");
+  }, [userData.user, history]);
+
   return (
     <div>
-      <h1>hello form signin</h1>
+      <form onSubmit={submit}>
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" onChange={onChange} />
+        <label htmlFor="password">Password</label>
+        <input type="text" name="password" onChange={onChange} />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 };
 
-export default SignIn;
+export default Login;
